147be488aabff92f386dfe956cb2bd564ddbffdb
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-25 11:33:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(object => object.result === "W")
+    return win? win.year : undefined 
+} 
